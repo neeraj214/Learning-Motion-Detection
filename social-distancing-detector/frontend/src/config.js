89253f02622876aafd43bb9492398a57929b1121
neeraj214 <150280096+neeraj214@users.noreply.github.com>
2026-04-12 21:41:33 +0530

    phase4: add API config

---
+++ social-distancing-detector/frontend/src/config.js
@@ -0,0 +1,4 @@
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+export const VIDEO_FEED_URL = `${BASE_URL}/video_feed`;
+export const EVENTS_URL     = `${BASE_URL}/events`;
+export const STATS_URL      = `${BASE_URL}/stats`;
